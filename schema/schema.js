504e8cdb2da825fa905b16f45c58a644e1124db1
Lucas Merchant <lbmerchant93@gmail.com>
2021-05-05 17:14:23 -0600

    Create more fields for JournalEntryType, create field for journalEntries to retrieve all entries

--- schema/schema.js
+++ schema/schema.js
@@ -6,15 +6,16 @@ const {
     GraphQLInt,
     GraphQLString,
     GraphQLBoolean,
+    GraphQLList,
     GraphQLObjectType,  
     GraphQLSchema 
 } = graphql;
 
 // dummy data
 let journalEntries = [
-    {id: '1', date: '03/05/2021', waterIntake: 80, prenatalVitamins: true, probiotics: false},
-    {id: '2', date: '04/05/2021', waterIntake: 75, prenatalVitamins: true, probiotics: true},
-    {id: '3', date: '05/05/2021', waterIntake: 85, prenatalVitamins: false, probiotics: true},
+    {id: '1', date: '03/05/2021', waterIntake: 80, prenatalVitamins: true, probiotics: false, proteinIntake: 50, exercise: 30, kegels: 50, garlandPose: 6},
+    {id: '2', date: '04/05/2021', waterIntake: 75, prenatalVitamins: true, probiotics: true, proteinIntake: 60, exercise: 10, kegels: 60, garlandPose: 7},
+    {id: '3', date: '05/05/2021', waterIntake: 85, prenatalVitamins: false, probiotics: true, proteinIntake: 70, exercise: 20, kegels: 70, garlandPose: 8},
 ];
 
 const JournalEntryType = new GraphQLObjectType({
@@ -24,7 +25,11 @@ const JournalEntryType = new GraphQLObjectType({
         date: { type: GraphQLString },
         waterIntake: { type: GraphQLInt },
         prenatalVitamins: {type: GraphQLBoolean },
-        probiotics: { type: GraphQLBoolean }
+        probiotics: { type: GraphQLBoolean },
+        proteinIntake: { type: GraphQLInt },
+        exercise: { type: GraphQLInt },
+        kegels: { type: GraphQLInt },
+        garlandPose: { type: GraphQLInt },
     })
 });
 
@@ -38,7 +43,13 @@ const RootQuery = new GraphQLObjectType({
                 //code to get data from db / other source
                 return _.find(journalEntries, { id: args.id });
             }
-        }
+        },
+        journalEntries: {
+            type: new GraphQLList(JournalEntryType),
+            resolve(parent, args) {
+                return journalEntries
+            }
+        },
     }
 })
 
